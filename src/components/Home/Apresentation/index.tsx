e82e3d8d833e952b7c8cf5ead9a4a17b7485cd5f
NanoThecnolog <contato@ericssongomes.com>
2025-09-10 13:37:15 -0300

    Adiciona estilos e video em timelapse ao fundo do componente Apresentation

--- src/components/Home/Apresentation/index.tsx
+++ src/components/Home/Apresentation/index.tsx
@@ -3,7 +3,31 @@ import styles from './styles.module.scss'
 export default function Apresentation() {
     return (
         <section className={styles.container}>
-            Sessão com apresentação rápida sobre a empresa
+            <video
+                className={styles.videoBackground}
+                autoPlay
+                loop
+                muted
+                playsInline
+            >
+                <source src="/video/timelapse.mp4" type="video/mp4" />
+                Seu navegador não suporta vídeos em HTML5.
+            </video>
+
+            <div className={styles.overlay}></div>
+            <div className={styles.content}>
+                <div className={styles.logomarca}>
+                    <img src="/img/LOGO.png" alt="Logomarca Engemarco" />
+                    <h1>Engemarco LTDA</h1>
+                </div>
+                <p>
+                    Desde 2024, atuamos com qualidade, segurança e seriedade em obras, reformas, cálculos estruturais, avaliações de imóveis, perícias de engenharia civil e segurança do trabalho.
+                    Hoje, somos reconhecidos e requisitados por empresas nacionais, multinacionais e tribunais de justiça do Rio de Janeiro.
+                </p>
+                <a href="#servicos" className={styles.cta}>
+                    Conheça nossos serviços
+                </a>
+            </div>
         </section>
     )
 }
