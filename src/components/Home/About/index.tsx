ab1ae31372495d9f1761f33dad4a106206f37f03
NanoThecnolog <contato@ericssongomes.com>
2025-09-11 09:51:48 -0300

    Adiciona informatcoes e estilos ao componente about

--- src/components/Home/About/index.tsx
+++ src/components/Home/About/index.tsx
@@ -5,9 +5,26 @@ export default function About() {
         <section className={styles.container} id='about'>
             <div className={styles.right}>
                 <h2>Quem Somos</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla explicabo exercitationem nostrum et amet, commodi ducimus assumenda maxime corrupti voluptates doloremque voluptatem, quis dolore dolor! Et itaque deserunt eaque laborum!</p>
+                <p>
+                    Em 2024, surgiu a <strong>Engemarco Sociedade LTDA</strong>, com o objetivo de
+                    atender seus clientes com qualidade e segurança. Aliando seriedade e
+                    competência à alta qualidade dos serviços prestados, a empresa cresceu e
+                    conquistou novos clientes.
+                </p>
+                <p>
+                    Hoje, somos requisitados por empresas nacionais e multinacionais, bem como
+                    pelos tribunais de justiça federal e estadual do Rio de Janeiro, para atividades
+                    que vão desde obras e reformas até cálculos estruturais, avaliação de imóveis,
+                    perícias de engenharia civil e segurança do trabalho.
+                </p>
+                <p>
+                    Nosso compromisso é oferecer soluções completas e confiáveis em engenharia,
+                    sempre priorizando a segurança, a inovação e a satisfação dos nossos clientes.
+                </p>
             </div>
-            <div className={styles.left}></div>
+            <div className={styles.left}>
+
+            </div>
         </section>
     )
 }
