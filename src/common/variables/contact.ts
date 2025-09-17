42d7efd50f16c82b4e02dfc176c858dd41834ba3
NanoThecnolog <contato@ericssongomes.com>
2025-09-17 13:58:50 -0300

    Altera email na variavel contact

--- src/common/variables/contact.ts
+++ src/common/variables/contact.ts
@@ -1,5 +1,5 @@
 export const contact = {
-    email: 'contato@engemarco.com',
+    email: 'contato@engemarcosolucoes.com',
     phone: '(21) 97207-0427',
     address: 'Icaraí - Niterói, RJ',
     mapAddress: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.395979421992!2d-43.105115124695376!3d-22.90457873856067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9983a729f4f265%3A0x72e57c3e5fb84e51!2sRua%20Coronel%20Moreira%20C%C3%A9sar%20-%20Icara%C3%AD%2C%20Niter%C3%B3i%20-%20RJ!5e0!3m2!1spt-BR!2sbr!4v1726169876543!5m2!1spt-BR!2sbr<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1489.8828082941861!2d-43.10890244626754!3d-22.901382544525614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9983e57a811495%3A0xdb1a120d231887ae!2sAv.%20Roberto%20Silveira%2C%20111%20-%20Icara%C3%AD%2C%20Niter%C3%B3i%20-%20RJ%2C%2024230-150!5e0!3m2!1spt-BR!2sbr!4v1757703543575!5m2!1spt-BR!2sbr',
